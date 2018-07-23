95a41582139669a890e6e7947eff5b52963e1058
Mic <shinedeyouxiang@foxmail.com>
2018-07-23 11:45:46 +0800

    v1.0.2

--- app/src/main/java/com/example/com/testrelaselib/MainActivity.kt
+++ app/src/main/java/com/example/com/testrelaselib/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.com.testrelaselib
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+
+    companion object {
+        fun enterFooLib(ctx: Context) {
+            ctx.startActivity(Intent(ctx, MainActivity::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
